c1d8e17c0bcf2e31e5bcaf29bb87e485dbe154e1
olivier <olivier@micoli.org>
2014-04-07 23:24:08 +0000

    firebug + orm

--- lib/classes/QDOrm.php
+++ lib/classes/QDOrm.php
@@ -29,6 +29,23 @@ function pub_testOrm($o){
 		'limit' => 2,
 	));
 	db($r);
+	$r = $oIPR->get(array(
+		'cols'	=> array('*'),
+		'where'	=> array(
+				'IPR_COMPANY_NAME'		=> array('IN',array("SPIE EST",'SNEF')),
+				'AND'					=> array(
+					'OPE1'					=> array("SQL","IPR_COMPANY_NAME like '%e%'"),
+					'OPE2'					=> array("SQL","IPR_COMPANY_NAME like '%a%'"),
+				),
+				'AND_2'					=> array(
+					'OPE1'					=> array("SQL","IPR_COMPANY_NAME like '%e%'"),
+					'OPE2'					=> array("SQL","IPR_COMPANY_NAME like '%a%'"),
+				)
+		),
+		'debugsql'	=> 1,
+		'start'		=> 0,
+		'limit'		=> 2,
+	));
 }
 
  */
@@ -82,6 +99,45 @@ class QDOrm{
 		return $col;
 	}
 
+	function where($o,&$query,&$aVal,$joinOperator,$level){
+		$indent="\n".str_pad("\t",$level*2);
+		foreach($o as $col=>$val){
+			if(preg_match('!^(OR|AND)(_[0-9]*){0,1}$!',$col,$m)){
+				$query.=$indent.$joinOperator.'(';
+				$this->where($val,$query,$aVal,$m[1]=='OR'?'AND':'OR',$level+1);
+				$query.=$indent.")";
+			}else{
+				$operator=false;
+				if(!is_array($val)){
+					$val=array('=',$val);
+				}
+				$operator = $val[0];
+				if($operator=='SQL'){
+					$operator=false;
+				}
+
+				if($operator== 'IN' ){
+					$t='';
+					$inSepa='';
+					foreach($val[1] as $v){
+						$t .= $inSepa . "'" . addslashes($v) . "'";
+						$inSepa=',';
+					}
+					$val[1]=sprintf('%s IN (%s) ',$this->_map($col),$t);
+					$operator=false;
+				}
+
+				if($operator){
+					$query .= sprintf("%s %s(%s %s :prm_col_%s) ",$sepa,$indent,$this->_map($col),$operator,count($aVal));
+					$aVal[':prm_col_'.count($aVal)] = $val[1];
+				}else{
+					$query .= sprintf("%s %s(%s) ",$sepa,$indent,$val[1]);
+				}
+				$sepa	= $indent.$joinOperator." ";
+				$k++;
+			}
+		}
+	}
 	/**
 	 *
 	 * @param unknown $o
@@ -110,53 +166,40 @@ class QDOrm{
 		$k=0;
 		$sepa='';
 		$aVal=array();
-		foreach($o['where'] as $col=>$val){
-			$operator=false;
-			if(!is_array($val)){
-				$operator="=";
-			}else{
-				$operator=$val[0];
-				if($operator=='SQL'){
-					$operator=false;
-				}
-				if($operator== 'IN' ){
-					$t='';
-					$inSepa='';
-					foreach($val[1] as $v){
-						$t.=$sepa."'".addslashes($v)."'";
-						$inSepa=',';
-					}
-					$val[1]=sprintf('%s IN (%s) ',$this->_map($col),$t);
-					$operator=false;
-				}
-			}
-			if($operator){
-				$query .= sprintf("%s \n%s %s :prm_col_%s ",$sepa,$this->_map($col),$operator,$k);
-				$aVal[':prm_col_'.$k]=$val;
-			}else{
-				$query .= sprintf("%s \n %s ",$sepa,$val[1]);
-			}
-			$sepa	= " AND ";
-			$k++;
+		$where = '';
+
+		$this->where(akead('where',$o,array()),$where,$aVal,'AND',0);
+
+		if(array_key_exists('wheresql',$o)){
+			$query .= 	$o['wheresql'];
+		}else{
+			$query .= ($where=='')?' TRUE ':$where;
 		}
 
-		if($k==0){
-			$query.=' TRUE ';
-		}
 		if(array_key_exists('limit',$o)){
 			$query .= sprintf("\nLIMIT %s,%s",akead('start',$o,0),$o['limit']);
 		}
+
+		/*foreach($aVal as $key=>$val){
+			if(!is_null($val)){
+				db($key."=>".json_encode($val));
+				$query = str_replace($key,'"'+addslashes($val)+"'",$query);
+			}
+		}*/
+
 		$stmt = $this->getConnection()->prepare($query);
 
 		foreach($aVal as $key=>$val){
 			if(!is_null($val)){
 				$stmt->bindValue($key, $val);
+			}else{
+				$stmt->bindValue($key, null,PDO::PARAM_NULL);
 			}
 		}
+
 		if($debugsql){
-			db($query);
+			fb($query."\n".json_encode($aVal));
 		}
-		//db($aVal);
 
 		$this->getConnection()->setAttribute(PDO::ATTR_CASE,PDO::CASE_LOWER);
 
@@ -221,7 +264,7 @@ class QDOrm{
 		}
 
 		if($debugsql){
-			print $query."\n";
+			fb($query);
 		}
 		if ($stmt->execute()){
 			if($mode=='update'){
@@ -233,4 +276,99 @@ class QDOrm{
 			throw new Exception(json_encode($stmt->errorInfo()),1);
 		}
 	}
+
+	public function __get($o){
+		$debugsql=false;
+		if(akead('debugsql',$o,false)){
+			$debugsql=true;
+			unset($o['debugsql']);
+		}
+		$o = array_merge(array(
+				'cols'	=> array('*'),
+				'where'	=> array()
+		),$o);
+
+		$cols='';
+		$sepa='';
+		foreach($o['cols'] as $k=>$v){
+			$cols.= $sepa.$this->_map($v);
+			$sepa =' , ';
+		}
+
+		$query= sprintf("SELECT %s \nFROM %s TFROM \nWHERE ",$cols,$this->table);
+
+		$k=0;
+		$sepa='';
+		$aVal=array();
+		foreach($o['where'] as $col=>$val){
+			$operator=false;
+			if(!is_array($val)){
+				$val=array('=',$val);
+			}
+			$operator = $val[0];
+			if($operator=='SQL'){
+				$operator=false;
+			}
+			if($operator== 'IN' ){
+				$t='';
+				$inSepa='';
+				foreach($val[1] as $v){
+					$t .= $inSepa . "'" . addslashes($v) . "'";
+					$inSepa=',';
+				}
+				$val[1]=sprintf('%s IN (%s) ',$this->_map($col),$t);
+				$operator=false;
+			}
+
+			if($operator){
+				$query .= sprintf("%s \n%s %s :prm_col_%s ",$sepa,$this->_map($col),$operator,$k);
+				$aVal[':prm_col_'.$k] = $val[1];
+			}else{
+				$query .= sprintf("%s \n %s ",$sepa,$val[1]);
+			}
+			$sepa	= " AND ";
+			$k++;
+		}
+
+		if(array_key_exists('wheresql',$o)){
+			$query .= 	$o['wheresql'];
+		}else{
+			if($k==0){
+				$query .= ' TRUE ';
+			}
+		}
+		if(array_key_exists('limit',$o)){
+			$query .= sprintf("\nLIMIT %s,%s",akead('start',$o,0),$o['limit']);
+		}
+
+		/*foreach($aVal as $key=>$val){
+		if(!is_null($val)){
+		db($key."=>".json_encode($val));
+		$query = str_replace($key,'"'+addslashes($val)+"'",$query);
+		}
+		}*/
+
+		$stmt = $this->getConnection()->prepare($query);
+
+		foreach($aVal as $key=>$val){
+			if(!is_null($val)){
+				$stmt->bindValue($key, $val);
+			}else{
+				$stmt->bindValue($key, null,PDO::PARAM_NULL);
+			}
+		}
+
+		if($debugsql){
+			fb($query."\n".json_encode($aVal));
+		}
+
+		$this->getConnection()->setAttribute(PDO::ATTR_CASE,PDO::CASE_LOWER);
+
+		if ($stmt->execute()){
+			return $stmt->fetchAll(PDO::FETCH_ASSOC);
+		}else{
+			throw new Exception(json_encode($stmt->errorInfo()),1);
+		}
+	}
+
 }
